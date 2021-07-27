bdca3dfce75b43597a56f046fc03d6abe345acdf
Vincent <vincesuuu1@gmail.com>
2021-07-27 14:02:34 -0700

    Completed the

--- Project/W4D2/class_inhe.rb
+++ Project/W4D2/class_inhe.rb
@@ -1,1 +1,43 @@
-"hello"
+class Employee
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+  end
+
+  def bonus(multiplier)
+    bonus = @salary * multiplier
+  end
+
+  
+  attr_reader :salary
+end
+
+class Manager < Employee
+
+  def initialize(name, title, salary, boss, employees)
+    @employees = employees
+    super(name, title, salary, boss)
+  end
+
+  def bonus(multiplier)
+    sum = 0
+    @employees.each do |ele|
+      sum += ele.salary
+    end
+    sum * multiplier
+  end
+
+end
+
+david = Employee.new("David", "TA", 10000, "Darren")
+shawna = Employee.new("Shawna", "TA", 12000, "Darren")
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned", [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren, shawna, david])
+
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
