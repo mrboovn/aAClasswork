9c3a9ac1a1e58a23f46faffaea6f8639f179c550
Minh Nguyen <m9nguyen@ucsd.edu>
2021-08-02 16:32:42 -0700

    switch

--- Project/W5D1/skeleton/lib/p01_int_set.rb
+++ Project/W5D1/skeleton/lib/p01_int_set.rb
@@ -70,26 +70,38 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader  :store, :num_buckets
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
-    @count = 0
+    @num_buckets = num_buckets
+    # @count = 0
   end
 
+  def count 
+    count = 0
+    @store.each do |bucket|
+      count += bucket.count
+    end
+    count
+  end
   def insert(num)
+    return false if self[num].include?(num)
+    self[num] << num 
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)    
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
@@ -97,5 +109,20 @@ class ResizingIntSet
   end
 
   def resize!
+    #@num_buckets = @num_buckets * 2
+  #   self.num_buckets.times do 
+  #     @store << []
+  #   end
+    puts "hello"
+    copied = self.store
+    self.store = Array.new(num_buckets*2) { Array.new}
+    copied.each do |bucket|
+      bucket.each do |num|
+        self[num] << num 
+      end
+    end
+    # puts "hello"
+    # puts self.store.length
   end
 end
+
