407fc2bd6816a5a173d3dfd27ca6772fefdd321e
your name here <your_email_hereroelliga@yahoo.com>
2021-08-13 14:00:25 -0700

    model

--- Project/pj99Cat/app/models/cat.rb
+++ Project/pj99Cat/app/models/cat.rb
@@ -1,5 +1,9 @@
 require 'action_view'
 class Cat < ApplicationRecord
+  include  ActionView::Helpers::DateHelper
+  validates :color, :birth_date, :name, :sex, presence: true
+  validates :sex, inclusion: %w(M F)
   def age 
+    time_ago_in_words(birth_date)
   end
 end
--- Project/pj99Cat/db/schema.rb
+++ Project/pj99Cat/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_193319) do
+ActiveRecord::Schema.define(version: 2021_08_13_204929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2021_08_13_193319) do
     t.date "birth_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "sex", limit: 1, null: false
+    t.string "color", null: false
+    t.text "description", null: false
     t.index ["birth_date"], name: "index_cats_on_birth_date"
   end
 
