bb9d08346eba9c32f304871abad87f2935c5fb26
your name here <your_email_hereroelliga@yahoo.com>
2021-08-13 14:42:17 -0700

    cats_controller

--- Project/pj99Cat/app/controllers/cats_controller.rb
+++ Project/pj99Cat/app/controllers/cats_controller.rb
@@ -9,9 +9,35 @@ class Cat < ApplicationController
     render :index 
   end
 
-  
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render json: @cat.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+    if @cat.update(artwork_params)
+      redirect_to artwork_url(@cat)
+    else  
+      render json: @cat.errors.full_messages, status: 422
+    end
+  end
+
+  def edit 
+    @cat = Cat.find(params[:id])
+    render :edit 
+  end 
 
   private 
   def cat_params 
-    params.require(:birth_date, :color, :age, :sex, :description)
+    params.require(:cat).permit(:birth_date, :color, :age, :sex, :description, :name)
 end
